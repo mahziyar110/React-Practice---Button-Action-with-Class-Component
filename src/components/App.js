2682f56e07d68d66f478586aacdc97900bbdf2b4
mahziyar110 <mahziyar110@gmail.com>
2021-08-11 01:29:15 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class Para extends Component {
-	render() {
-		return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-	}
-}
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {count: 0};
+		this.state = { count: 0 };
 	};
 
 	render() {
 		return (
 			<div id="main">
 				{ /* Do not remove this main div!! */}
-				<button id="click" onClick={ () => {
-					this.setState({count: this.state.count + 1});
-					return <Para/>
+				<button id="click" onClick={() => {
+					this.setState({ count: this.state.count + 1 });
+					return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
 				}} >Render Paragraph</button>
 			</div>
 		);
