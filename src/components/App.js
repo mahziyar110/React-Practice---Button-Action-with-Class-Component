23a8f90b4ab37db53517925edd3f7009ec9fe168
mahziyar110 <mahziyar110@gmail.com>
2021-08-11 01:03:09 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+class Para extends Component {
+	render() {
+		return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+	}
+}
+
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -11,7 +17,7 @@ class App extends Component {
 			<div id="main">
 				{ /* Do not remove this main div!! */}
 				<button id="click" onClick={function () {
-					return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+					return <Para/>
 				}} >Render Paragraph</button>
 			</div>
 		);
