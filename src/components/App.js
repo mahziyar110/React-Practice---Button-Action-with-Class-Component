8a14a7b4121a0635b5cd28acc0a4a535f270d8e6
mahziyar110 <mahziyar110@gmail.com>
2021-08-11 00:57:39 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{ /* Do not remove this main div!! */}
+				<button id="click" onClick={function () {
+					return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				}} >Render Paragraph</button>
+			</div>
+		);
+	}
 }
 
 
